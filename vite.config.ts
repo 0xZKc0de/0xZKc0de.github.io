e8a4c2e4eac90da4129b971be70630a50af83ee4
0xZKc0de <elhaddadmohamed963@gmail.com>
2025-09-18 16:01:48 +0200

    Update vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/b3dr0ck-port-builder/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
